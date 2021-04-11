52235a842a36ed6a4f2f3147f05932791a156995
Willian Takanage <80928807+takanage@users.noreply.github.com>
2021-04-10 23:51:50 -0300

    Finalizado
    Finalizado exercício, acrescentado emojis diretamente no script.

--- script.js
+++ script.js
@@ -20,17 +20,50 @@ function isLista(n, l) {
 }   
 function adicionar () { 
     if (isNumero(num.value) && !isLista(num.value, valores)) {
-         var n = Number(num.value)
-         tab.innerHTML = ''
+         var n = Number(num.value)    
          let item = document.createElement('option')
          item.text = `Valor ${n} adicionado.`
          item.value = `tab${n}`
          tab.appendChild(item)
          valores.push(n)
+         res.innerHTML = ''
+
          
     } else {
         alert('Número Inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.<p>`
+        res.innerHTML += `<p> ➕ O maior valore é: ${maior}.<p>`
+        res.innerHTML += `<p> ➖ O menor valore é: ${menor}.<p>`
+        res.innerHTML += `<p> 🧮 A soma dos valores é: ${soma}.<p>`
+        res.innerHTML += `<p> Ⓜ️ A média dos valores é: ${media.toFixed(2).replace(".", ",")}.<p>` 
+
+
+    }
+
+
 }
 
 
